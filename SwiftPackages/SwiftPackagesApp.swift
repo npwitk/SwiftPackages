0faaae71e5cbd961f7b0b7993419e46751729e27
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-06-02 20:12:20 +0700

    Initial Commit

---
+++ SwiftPackages/SwiftPackagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPackagesApp.swift
+//  SwiftPackages
+//
+//  Created by Nonprawich I. on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPackagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
